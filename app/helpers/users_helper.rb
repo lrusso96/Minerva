958eab6ee4e959c85c0df16b70d69e7e00f6ff67
lrusso96 <russo.1699981@studenti.uniroma1.it>
2018-05-02 12:47:07 +0200

    simple links in header, profile page with really basic infos

---
+++ app/helpers/users_helper.rb
@@ -0,0 +1,20 @@
+module UsersHelper
+  # Returns the Gravatar for the given user.
+  def gravatar_for(user, options = { size: 80 })
+    gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
+    size = options[:size]
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    image_tag(gravatar_url, alt: user.surname, class: 'profile_profile_pic')
+  end
+
+  # Returns the Gravatar for the given user.
+  def gravatar_url_for(user, options = { size: 80 })
+    gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
+    size = options[:size]
+    "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+  end
+
+  def registration_day_of(user)
+    user.created_at.strftime('%B %d, %Y')
+  end
+end
