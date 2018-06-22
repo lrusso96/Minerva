d301cbf863af3581214a76ce5601b69d9bf1a52e
lrusso96 <russo.1699981@studenti.uniroma1.it>
2018-06-22 20:18:14 +0200

    basic Arxiv search
    It is now possible to make simple searches over Arxiv db. This feature is still in develop and some bugs will be fixed later on

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -16,7 +16,10 @@ class StaticPagesController < ApplicationController
   end
 
   def explore
+    query = params[:query]
+    @found_papers = MinervaApi::Arxiv.search(query) if query
     @top_papers = Paper.order(:likers_count).last(5).reverse
     @recent_papers = Paper.order(:created_at).last(5).reverse
   end
+
 end
