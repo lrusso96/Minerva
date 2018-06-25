ef2c81ad166503fbc3a76fd763990ef2f2162a4f
lrusso96 <russo.1699981@studenti.uniroma1.it>
2018-06-25 18:21:19 +0200

    minor fix

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,2 +1,13 @@
 module StaticPagesHelper
+  def shorten_string(s, l)
+    s[0..l] + '...'
+  end
+
+  def shorten_title(p)
+    shorten_string p.title, 40
+  end
+
+  def shorten_abstract(p)
+    shorten_string p.description, 80
+  end
 end
