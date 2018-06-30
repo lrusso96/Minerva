144b0700333e93c25f0ea7f24358eec59ef1f5f7
MatteoSalvino <salvino.1708108@studenti.uniroma1.it>
2018-06-30 18:55:19 +0200

    Login, forgot password and contact design

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -6,7 +6,7 @@ class StaticPagesController < ApplicationController
     @feed_papers = Paper.where(user_id: followees.map(&:id)).last(5).reverse
     @starred_papers = current_user.likees(Paper).last(5).reverse
     @followers = current_user.followers(User).reverse
-    @followees = current_user.followees(User).last(5).reverse
+    @followees = current_user.followees(User).reverse
   end
 
   def help
